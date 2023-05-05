35921a00a684c9954034218da3fa4118524396c6
thousandmiles <69280552+thousandmiles@users.noreply.github.com>
2023-05-05 19:50:56 +0800

    get software class,title,text

---
+++ scrapy/main.py
@@ -0,0 +1,19 @@
+from bs4 import BeautifulSoup
+import requests
+
+url = 'https://www.rockwellautomation.com/en-us/products.html'
+
+response = requests.get(url)
+
+res = BeautifulSoup(response.text, 'lxml')
+
+class_list = res.find_all(attrs={'class': 'generic-container push-top-full push-bottom-full aem-GridColumn aem-GridColumn--default--12'})
+
+for child in class_list:
+    text = child.find_all(attrs = {'class': 'teaser__text'})
+    title = child.find_all(attrs = {'class': 'teaser__title'})
+    class_ = child.find(attrs={'class': 'ff-primary-bold-rte subheading-3 color-factory-talk-blue'}).get_text().strip()
+    for i in range(len(text)):
+        text_ = text[i].get_text().strip()
+        title_ = title[i].get_text().strip()
+        print( class_, "-----", title_, "-----", text_)
