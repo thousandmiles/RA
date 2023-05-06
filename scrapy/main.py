85e760d43e5bfba93c75b22b81f2bfc5300a950a
thousandmiles <69280552+thousandmiles@users.noreply.github.com>
2023-05-06 23:42:20 +0800

    class usage

--- scrapy/main.py
+++ scrapy/main.py
@@ -8,39 +8,26 @@ response = requests.get(all_products_url)
 
 res = BeautifulSoup(response.text, 'lxml')
 
-# All Software 
+# All Software
 
-all_soft = [] 
+all_soft = []
 
-class_list = res.find_all(attrs={'class': 'generic-container push-top-full push-bottom-full aem-GridColumn aem-GridColumn--default--12'})
+class_list = res.find_all(
+    attrs={'class': 'generic-container push-top-full push-bottom-full aem-GridColumn aem-GridColumn--default--12'})
 
 for child in class_list:
-    text = child.find_all(attrs = {'class': 'teaser__text'})
-    title = child.find_all(attrs = {'class': 'teaser__title'})
+    text = child.find_all(attrs={'class': 'teaser__text'})
+    title = child.find_all(attrs={'class': 'teaser__title'})
     ref = child.find_all('a')
-    class_ = child.find(attrs={'class': 'ff-primary-bold-rte subheading-3 color-factory-talk-blue'}).get_text().strip()
-    
+    class_ = child.find(attrs={
+                        'class': 'ff-primary-bold-rte subheading-3 color-factory-talk-blue'}).get_text().strip()
+
     for i in range(len(text)):
         text_ = text[i].get_text().strip()
         title_ = title[i].get_text().strip()
         ref_ = ref[i]['href'].strip()
 
-        soft_info = [class_, title_, text_, ref_]
+        soft_info = tool.Software(class_, title_, text_, ref_)
+        soft_info.CompleteURL()
+        soft_info.GetBannerInfo()
         all_soft.append(soft_info)
-
-# for item in all_soft:
-#     for i in item:
-#         print(i)
-#     print("----------------")
-
-# Single software
-
-# banner__header & banner__description
-single_software_url = 'https://www.rockwellautomation.com/en-us/products/software/factorytalk/operationsuite/metrics.html'
-
-for item in all_soft:
-    url = item[3]
-    if url.startswith("/en"):
-        url = "https://www.rockwellautomation.com" + url
-        tool.GetBannerInfo(url)
-
