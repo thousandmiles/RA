f6e64524e5f8b412ffc6eca952a7b3fdccf6d8e8
thousandmiles <69280552+thousandmiles@users.noreply.github.com>
2023-05-06 23:42:47 +0800

    add software class

--- scrapy/tool.py
+++ scrapy/tool.py
@@ -2,23 +2,37 @@ from bs4 import BeautifulSoup
 import requests
 
 
-def GetBannerInfo(url):
-    print('--------------------')
-    print('URL = ',url)
+class Software(object):
 
-    s_response = requests.get(url)
-    s_res = BeautifulSoup(s_response.text, 'lxml')
-    
-    info = s_res.find(attrs = {'class': 'hero-banner__content'})
-    if info is None:
-        return
+    def __init__(self, class_, title_, text_, ref_):
+        self.class_ = class_
+        self.title_ = title_
+        self.text_ = text_
+        self.ref_ = ref_
 
-    banner__header = info.find(attrs = {'class': 'hero-banner__header'}).get_text().strip()
-    if banner__header is None:
-        return
-    print('Header = ', banner__header)
+    def CompleteURL(self):
+        if self.ref_.startswith("/en"):
+            self.ref_ = "https://www.rockwellautomation.com" + self.ref_
 
-    banner__description = info.find(attrs = {'class': 'hero-banner__description subheading-1'}).get_text().strip()
-    if banner__description is None:
-        return
-    print('Description = ', banner__description)
+    def GetBannerInfo(self):
+        print('--------------------')
+        print('URL = ', self.ref_)
+
+        s_response = requests.get(self.ref_)
+        s_res = BeautifulSoup(s_response.text, 'lxml')
+
+        info = s_res.find(attrs={'class': 'hero-banner__content'})
+        if info is None:
+            return
+
+        self.banner__header = info.find(
+            attrs={'class': 'hero-banner__header'}).get_text().strip()
+        if self.banner__header is None:
+            return
+        print('Header = ', self.banner__header)
+
+        self.banner__description = info.find(
+            attrs={'class': 'hero-banner__description subheading-1'}).get_text().strip()
+        if self.banner__description is None:
+            return
+        print('Description = ', self.banner__description)
