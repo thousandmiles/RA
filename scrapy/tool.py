c17cf433b59d929f2d7c7d963e2e8b2912e272de
thousandmiles <69280552+thousandmiles@users.noreply.github.com>
2023-05-09 12:36:24 +0800

    publication, bg info

--- scrapy/tool.py
+++ scrapy/tool.py
@@ -36,13 +36,25 @@ class Software(object):
 
     def GetBackgroundImage(self):
         self.bg_url = ''
+        self.bg_info = ''
         content_bg = self.res.find(attrs={'class': 'main-image'})
         if content_bg is not None:
             self.bg_url = content_bg['src'].strip()
+            self.bg_info = content_bg['alt'].strip()
             if (self.bg_url.endswith('.jpg') or self.bg_url.endswith('.jpeg') or self.bg_url.endswith('.png')):
                 pass
             else:
                 self.bg_url = ''
+
+    def GetPublication(self):
+        self.publication = ''
+        content_pub = self.res.find(
+            attrs={'class': 'generic-container__inner'})
+        if content_pub is not None:
+            pub = content_pub.find(
+                attrs={'class': 'publication'})
+            if pub is not None:
+                self.publication = pub.get_text().strip()
 
     def PrintInfo(self):
         print('--------------------')
@@ -53,3 +65,5 @@ class Software(object):
         print('Header = ', self.banner__header)
         print('Description = ', self.banner__description)
         print('Bg URL = ', self.bg_url)
+        print('Bg Info = ', self.bg_info)
+        print('Publication = ', self.publication)
